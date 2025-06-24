43373fe54a8289f307847ebe987a435dea8bb632
AnLaKii <kaankursatkilic@gmail.com>
2025-06-24 21:13:26 +0300

    final commit

--- web api/routes/education.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { EgitimListele, EgitimlerimiListele} from "../controllers/education.js";
-export const router = Router();
-
-
-router.get("/",EgitimListele);
-router.get("/detail",EgitimlerimiListele);
